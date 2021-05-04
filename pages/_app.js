fe7b17bd64be87e7c1ac3f8e7ac9398dc8d68c1a
BuildTools <unconfigured@null.spigotmc.org>
2021-05-03 23:08:54 -0400

    Added Spotify integration

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-import {ThemeProvider} from 'next-themes'
+import { ThemeProvider } from "next-themes";
 
-function MyApp({Component, pageProps}) {
-
-    return (
-        <ThemeProvider attribute="class">
-            <Component {...pageProps} />
-        </ThemeProvider>
-    )
+// eslint-disable-next-line react/prop-types
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -1,55 +1,55 @@
-import Document, {Html, Head, Main, NextScript} from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class NextDocument extends Document {
-    render() {
-        return (
-            <Html lang="en">
-                <Head>
-                    <link
-                        rel="preload"
-                        href="/fonts/inter-var-latin.woff2"
-                        as="font"
-                        type="font/woff2"
-                        crossOrigin="anonymous"
-                    />
-                    <link href="/static/favicons/favicon.ico" rel="shortcut icon"/>
-                    <link href="/static/favicons/site.webmanifest" rel="manifest"/>
-                    <link
-                        href="/static/favicons/apple-touch-icon.png"
-                        rel="apple-touch-icon"
-                        sizes="180x180"
-                    />
-                    <link
-                        href="/static/favicons/favicon-32x32.png"
-                        rel="icon"
-                        sizes="32x32"
-                        type="image/png"
-                    />
-                    <link
-                        href="/static/favicons/favicon-16x16.png"
-                        rel="icon"
-                        sizes="16x16"
-                        type="image/png"
-                    />
-                    <link
-                        color="#4a9885"
-                        href="/static/favicons/safari-pinned-tab.svg"
-                        rel="mask-icon"
-                    />
-                    <meta content="#F9A8D4" name="theme-color"/>
-                    <meta content="#000000" name="msapplication-TileColor"/>
-                    <meta
-                        content="/static/favicons/browserconfig.xml"
-                        name="msapplication-config"
-                    />
-                </Head>
-                <body className="bg-black dark:bg-white text-white dark:text-black">
-                <Main/>
-                <NextScript/>
-                </body>
-            </Html>
-        );
-    }
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/inter-var-latin.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
+          <link href="/static/favicons/site.webmanifest" rel="manifest" />
+          <link
+            href="/static/favicons/apple-touch-icon.png"
+            rel="apple-touch-icon"
+            sizes="180x180"
+          />
+          <link
+            href="/static/favicons/favicon-32x32.png"
+            rel="icon"
+            sizes="32x32"
+            type="image/png"
+          />
+          <link
+            href="/static/favicons/favicon-16x16.png"
+            rel="icon"
+            sizes="16x16"
+            type="image/png"
+          />
+          <link
+            color="#4a9885"
+            href="/static/favicons/safari-pinned-tab.svg"
+            rel="mask-icon"
+          />
+          <meta content="#F9A8D4" name="theme-color" />
+          <meta content="#000000" name="msapplication-TileColor" />
+          <meta
+            content="/static/favicons/browserconfig.xml"
+            name="msapplication-config"
+          />
+        </Head>
+        <body className="bg-black dark:bg-white text-white dark:text-black">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 export default NextDocument;
